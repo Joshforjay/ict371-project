52576b5c7c33a35538e039f72ede65742b512137
Andrew <andrewdante2@gmail.com>
2022-05-10 22:00:33 +0800

    Level 3 - Camera collision (NOT WORKING)

--- Assets/Resources/Level3Assets/Scripts/PlayerMove3.cs
+++ Assets/Resources/Level3Assets/Scripts/PlayerMove3.cs
@@ -2,30 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove3 : MonoBehaviour
-{
-    public int moveSpeed = 70;
-    public Transform cam;
-    Rigidbody rb;
+public class PlayerMove3 : MonoBehaviour {
+	public int moveSpeed = 70;
+	public Transform cam;
+	Rigidbody rb;
 
-    private void Start()
-    {
-        Cursor.lockState = CursorLockMode.Confined;
-        rb = GetComponent<Rigidbody>();
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButton("Fire1"))
-        {
-            if (rb.velocity.magnitude < 100)
-                rb.velocity = transform.forward * moveSpeed;
-        }
-        else if (Input.GetButtonUp("Fire1"))
-           rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+	RaycastHit hit;
 
-        rb.MoveRotation(cam.rotation);
-    }
+	private void Start() {
+		Cursor.lockState = CursorLockMode.Confined;
+		rb = GetComponent<Rigidbody>();
+	}
+	// Update is called once per frame
+	void Update() {
+		if (Physics.Linecast(transform.position, cam.position, out hit)) {
+			cam.position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+		}
 
+		if (cam.localPosition.z > -3f) {
+			cam.localPosition = new Vector3(0f, 0f, -3f);
+		}
+
+		if (Input.GetButton("Fire1")) {
+			if (rb.velocity.magnitude < 100)
+				rb.velocity = transform.forward * moveSpeed;
+		} else if (Input.GetButtonUp("Fire1"))
+			rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+
+		rb.MoveRotation(cam.rotation);
+	}
 }
-
