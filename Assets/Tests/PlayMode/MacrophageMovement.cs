2378246ffac69d6f5b313fade7fd136718c90590
Joshforjay <joshforjay@gmail.com>
2022-05-13 21:52:25 +0800

    more testing

--- Assets/Tests/PlayMode/MacrophageMovement.cs
+++ Assets/Tests/PlayMode/MacrophageMovement.cs
@@ -1,20 +1,31 @@
+using ICTProject;
 using System.Collections;
 using UnityEngine;
+using UnityEditor;
+using UnityEngine.Assertions;
 using UnityEngine.InputSystem;
 using UnityEngine.TestTools;
 
 namespace Tests
 {
-    public class MacroDirectionTests : InputTestFramework
+    public class MacroDirectionTests : InputTestFixture
     {
-        public GameObject player;
+        public GameObject playerPrefab;
 
         [UnityTest]
         public IEnumerator TurnLeft()
         {
-            
+            var input = InputSystem.AddDevice<Keyboard>();
 
-            yield return null;
+            GameObject player = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Resources/Level1Assets/Prefab/Player");
+            GameObject go = Object.Instantiate(player);
+            float yRot = go.transform.rotation.y;
+            Debug.Log(yRot);
+            Press(input.aKey);
+
+            yield return 1f;
+            Debug.Log(go.transform.rotation.y);
+            Assert.IsTrue(go.transform.rotation.y < yRot);
         }
     }
 }
