aa65b484cf9dd02903637350d6dd134aed9c81e4
Joshforjay <joshforjay@gmail.com>
2022-05-13 20:11:37 +0800

    beginning unti tests

---
+++ Assets/Tests/PlayMode/MacrophageMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MacroDirectionTests : InputTestFramework
+    {
+        public GameObject player;
+
+        [UnityTest]
+        public IEnumerator TurnLeft()
+        {
+            
+
+            yield return null;
+        }
+    }
+}
